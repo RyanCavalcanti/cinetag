51f1c6fc94a60918a91f2706d91d2de6d25f68f5
Ryan Cavalcanti <64388037+RyanCavalcanti@users.noreply.github.com>
2023-10-26 14:45:14 -0300

    Add files via upload

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,9 @@
+import styles from './Banne.module.css';
+
+function Banner({ imagem }) {
+    return(
+        <div className={styles.capa} style={{backgroundImage: `url('/imagens/banner-${imagem}.png')`}}></div>
+    )
+}
+
+export default Banner
